d7cc8663e7abf5949aec649a1ec581621ac40d3d
Tara Wallace <tara@covarum.com>
2026-04-08 15:17:48 -0400

    Remove site chat widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Covarum",
@@ -19,16 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-
-        <Script
-          src="https://widgets.leadconnectorhq.com/loader.js"
-          strategy="afterInteractive"
-          data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js"
-          data-widget-id="69d29b580515dd8ed518b14f"
-        />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
